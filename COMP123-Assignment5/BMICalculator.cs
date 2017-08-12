e014170c6a48bd4fee8f0fd84abda5fb92d071a1
Angelaaa <Angelaaa@LAPTOP-0Q8KPPTN>
2017-08-11 20:02:59 -0400

    Added CalculateButton_Click method

--- COMP123-Assignment5/BMICalculator.cs
+++ COMP123-Assignment5/BMICalculator.cs
@@ -12,7 +12,7 @@ using System.Windows.Forms;
  * Name: Yanying Zhang - 300926213
  * Date: Aug.11 2017
  * Description: Assignment5
- * Version: 0.2 - Added the radioButton_Clicked()
+ * Version: 0.3 - Added CalculateButton_Click method
  */
 namespace COMP123_Assignment5
 {
@@ -66,6 +66,48 @@ namespace COMP123_Assignment5
             }
         }
 
-     
+        /// <summary>
+        /// This is the method calculate the BMI and show the string of BMI result
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            double weight = Convert.ToDouble(WeightInputTextBox.Text);
+            double height = Convert.ToDouble(HeightInputTextBox.Text);
+
+            double BMI;
+            string Result = "";
+            if (this.ImperialRadio.Checked)
+            {
+                BMI = Math.Round(((weight * 703) / (height * height)), 1);
+            }
+            else
+            {
+                BMI = Math.Round((weight / (height * height)), 1);
+            }
+
+            if (BMI < 18.5)
+            {
+                Result = "Underweight";
+            }
+            if (BMI >= 18.5 && BMI <= 24.9)
+            {
+                Result = "Normal";
+            }
+            if (BMI >= 25 && BMI <= 29.9)
+            {
+                Result = "Overweight";
+            }
+            if (BMI >= 30)
+            {
+                Result = "Obse";
+            }
+
+            this.BMITextBox.Text = BMI.ToString();
+            this.BMITextBox.Text = Convert.ToString(BMI);
+            this.BMIResultTextBox.Text = Result;
+        }
+
     }
 }
